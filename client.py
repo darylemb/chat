73043a89401a4f458e1ef0a73a988d9a940e9312
darylemb <58637116+darylemb@users.noreply.github.com>
2020-06-06 20:05:24 -0500

    Add files via upload

--- client.py
+++ client.py
@@ -1,8 +1,91 @@
+#!/usr/bin/env python3
+"""Script for Tkinter GUI chat client."""
 import socket
+from socket import AF_INET, SOCK_STREAM
+from threading import Thread
+from tkinter import *
+import os
 
-HOST = '127.0.0.1'
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect((HOST, 4000))
-while True:
-    sock.send(bytes(input(),encoding="utf-8"))
-sock.close()
+firstclick = True
+
+def on_entry_click(event):
+    """function that gets called whenever entry1 is clicked"""        
+    global firstclick
+
+    if firstclick: # if this is the first time they clicked it
+        firstclick = False
+        entry_field.delete(0, "end") # delete all the text in the entry
+
+
+def receive():
+    """Handles receiving of messages."""
+    while True:
+        try:
+            msg = client_socket.recv(BUFSIZ).decode("utf8")
+            if msg = 
+            msg_list.insert(END, msg)
+        except OSError:  # Possibly client has left the chat.
+            break
+
+
+def send(event=None):  # event is passed by binders.
+    """Handles sending of messages."""
+    msg = my_msg.get()
+    if msg == "/unicast":
+        my_msg.set("")
+        client_socket.send(bytes(msg,"utf8"))
+    my_msg.set("")  # Clears input field.
+    client_socket.send(bytes(msg, "utf8"))
+    if msg == "{quit}":
+        client_socket.close()
+        root.quit()
+    
+
+
+
+def on_closing(event=None):
+    """This function is to be called when the window is closed."""
+    my_msg.set("{quit}")
+    send()
+
+root = Tk()
+root.title("ChatIO")
+
+messages_frame = Frame(root)
+my_msg = StringVar()  # For the messages to be sent.
+my_msg.set("Type your messages here.")
+scrollbar = Scrollbar(messages_frame)  # To navigate through past messages.
+# Following will contain the messages.
+msg_list = Listbox(messages_frame, height=15, width=50, yscrollcommand=scrollbar.set)
+scrollbar.pack(side=RIGHT, fill=Y)
+msg_list.pack(side=LEFT, fill=BOTH)
+msg_list.pack()
+messages_frame.pack()
+
+entry_field = Entry(root, textvariable=my_msg)
+entry_field.bind('<FocusIn>', on_entry_click)
+entry_field.bind("<Return>", send)
+entry_field.pack()
+send_button = Button(root, text="Send", command=send)
+send_button.pack()
+
+root.protocol("WM_DELETE_WINDOW", on_closing)
+
+#----Socket code----
+HOST = os.popen('hostname -I').read().strip()
+PORT = None
+if not PORT:
+    PORT = 33002
+else:
+    PORT = int(PORT)
+
+BUFSIZ = 1024
+ADDR = (HOST, PORT)
+
+client_socket = socket.socket(AF_INET, SOCK_STREAM)
+client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+client_socket.connect(ADDR)
+
+receive_thread = Thread(target=receive)
+receive_thread.start()
+root.mainloop()
--- server.py
+++ server.py
@@ -1,15 +1,76 @@
+#!/usr/bin/env python3
+"""Server for multithreaded (asynchronous) chat application."""
 import socket
-import time
+from socket import AF_INET, SOCK_STREAM
+from threading import Thread
+import sys
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.bind(('127.0.0.1', 4000))
-sock.listen(5)
+def accept_incoming_connections():
+    """Sets up handling for incoming clients."""
+    while True:
+        client, client_address = SERVER.accept()
+        print("%s:%s has connected." % client_address)
+        client.send(bytes("Greetings from the cave! Now type your name and press enter!", "utf8"))
+        addresses[client] = client_address
+        Thread(target=handle_client, args=(client,)).start()
 
-while True:
-    clientsocket, address = sock.accept()
-    print("Connection is stablished", address)
-    clientsocket.send(bytes("You are connected",encoding="utf-8"))
-    r = clientsocket.recv(256)
-    print(r.decode("utf-8"))
-    clientsocket.close()
-sock.close()
+
+def handle_client(client):  # Takes client socket as argument.
+    """Handles a single client connection."""
+    name = client.recv(BUFSIZ).decode("utf8")
+    welcome = 'Welcome %s! If you ever want to quit, type {quit} to exit.' % name
+    client.send(bytes(welcome, "utf8"))
+    msg = "%s has joined the chat!" % name
+    broadcast(bytes(msg, "utf8"))
+    clients[client] = name
+
+    while True:
+        msg = client.recv(BUFSIZ)
+        #Aqui se tiene que diferenciar el unicast de multicast y broadcast
+        if msg =bytes("/"+,"utf8")
+        if msg != bytes("{quit}", "utf8"):
+            broadcast(msg, name+": ")
+        else:
+            client.send(bytes("{quit}", "utf8"))
+            client.close()
+            del clients[client]
+            if len(clients) == 0:
+                SERVER.close()
+                sys.exit(0)
+                break
+            broadcast(bytes("%s has left the chat." % name, "utf8"))
+            break
+        print(clients)
+
+
+def broadcast(msg, prefix=""):  # prefix is for name identification.
+    """Broadcasts a message to all the clients."""
+    #if msg = "/unicast":
+
+    for sock in clients:
+        sock.send(bytes(prefix, "utf8")+msg)
+
+#def multicast()
+#def unicast(msg,name):
+
+
+        
+clients = {}
+addresses = {}
+
+HOST = ''
+PORT = 33002
+BUFSIZ = 1024
+ADDR = (HOST, PORT)
+
+SERVER = socket.socket(AF_INET, SOCK_STREAM)
+SERVER.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+SERVER.bind(ADDR)
+
+if __name__ == "__main__":
+    SERVER.listen(5)
+    print("Waiting for connection...")
+    ACCEPT_THREAD = Thread(target=accept_incoming_connections)
+    ACCEPT_THREAD.start()
+    ACCEPT_THREAD.join()
+    SERVER.close()
